30b09f7ac8c15d76f138d6eeb0422dc9cc396535
ucaccsp <ucaccsp@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1998-06-22 16:47:00 +0000

    A couple-of small fixes for linux (I haven't added any grabber code yet, wait for linux-2.2 and the video4linux integration...)

--- config.h
+++ config.h
@@ -89,7 +89,7 @@ extern "C" {
 #include <arpa/inet.h>
 int strcasecmp(const char *, const char *);
 clock_t clock(void);
-#if !defined(sco) && !defined(sgi) && !defined(__bsdi__) && !defined(__FreeBSD__) && !defined(sun)
+#if !defined(sco) && !defined(sgi) && !defined(__bsdi__) && !defined(__FreeBSD__) && !defined(sun) && !defined(__linux__)
 int gethostid(void);
 #endif
 time_t time(time_t *);
