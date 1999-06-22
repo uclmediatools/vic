d3f90dee2f7f7616b12d052def1895d168899bfb
piers <piers@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
1999-06-22 12:22:00 +0000

    Added #ifdef win32 for inline function defs - which don't work in a C file under windows.

--- h263/h263rtp.c
+++ h263/h263rtp.c
@@ -53,14 +53,55 @@
 #include "defs.h"
 
 #include "h263.h"
- 
+
+/* Bit masks used by bit i/o operations. */
+
+unsigned int nBitMask[] = { 0x00000000, 0x80000000, 0xc0000000, 0xe0000000, 
+                            0xf0000000, 0xf8000000, 0xfc000000, 0xfe000000, 
+                            0xff000000, 0xff800000, 0xffc00000, 0xffe00000, 
+                            0xfff00000, 0xfff80000, 0xfffc0000, 0xfffe0000, 
+                            0xffff0000, 0xffff8000, 0xffffc000, 0xffffe000, 
+                            0xfffff000, 0xfffff800, 0xfffffc00, 0xfffffe00, 
+                            0xffffff00, 0xffffff80, 0xffffffc0, 0xffffffe0, 
+                            0xfffffff0, 0xfffffff8, 0xfffffffc, 0xfffffffe};
+
+unsigned int bitMask[] = {  0xffffffff, 0x7fffffff, 0x3fffffff, 0x1fffffff, 
+                            0x0fffffff, 0x07ffffff, 0x03ffffff, 0x01ffffff,
+                            0x00ffffff, 0x007fffff, 0x003fffff, 0x001fffff,
+                            0x000fffff, 0x0007ffff, 0x0003ffff, 0x0001ffff,
+                            0x0000ffff, 0x00007fff, 0x00003fff, 0x00001fff,
+                            0x00000fff, 0x000007ff, 0x000003ff, 0x000001ff,
+                            0x000000ff, 0x0000007f, 0x0000003f, 0x0000001f,
+                            0x0000000f, 0x00000007, 0x00000003, 0x00000001};
+
+unsigned int rBitMask[] = { 0xffffffff, 0xfffffffe, 0xfffffffc, 0xfffffff8, 
+                            0xfffffff0, 0xffffffe0, 0xffffffc0, 0xffffff80, 
+                            0xffffff00, 0xfffffe00, 0xfffffc00, 0xfffff800, 
+                            0xfffff000, 0xffffe000, 0xffffc000, 0xffff8000, 
+                            0xffff0000, 0xfffe0000, 0xfffc0000, 0xfff80000, 
+                            0xfff00000, 0xffe00000, 0xffc00000, 0xff800000, 
+                            0xff000000, 0xfe000000, 0xfc000000, 0xf8000000, 
+                            0xf0000000, 0xe0000000, 0xc0000000, 0x80000000};
+
+unsigned int bitTest[] = {  0x80000000, 0x40000000, 0x20000000, 0x10000000, 
+                            0x08000000, 0x04000000, 0x02000000, 0x01000000,
+                            0x00800000, 0x00400000, 0x00200000, 0x00100000,
+                            0x00080000, 0x00040000, 0x00020000, 0x00010000,
+                            0x00008000, 0x00004000, 0x00002000, 0x00001000,
+                            0x00000800, 0x00000400, 0x00000200, 0x00000100,
+                            0x00000080, 0x00000040, 0x00000020, 0x00000010,
+                            0x00000008, 0x00000004, 0x00000002, 0x00000001};
+
 #define HDRSIZE_A	4
 #define HDRSIZE_B	8
 
 /* FIXME: bitextraction can be done faster, yes */
-static inline u_long 
-extract_bits(const u_char *bp,const int x,const int n) {
-        u_long xx;
+#ifdef WIN32
+static u_long extract_bits(const u_char *bp,const int x,const int n) {
+#else 
+static inline u_long extract_bits(const u_char *bp,const int x,const int n) {
+#endif
+	u_long xx;
         int     i;
 #define BIT(x) !!(bp[x>>3]&(1<<(7-(x&7))))
 
@@ -98,7 +139,12 @@ void copy_buf_to_streamheader(const u_char *bp,h263streamheader *h263sh) {
 }
 
 #define SBIT(x,to) bp[x>>3]|=(to<<(7-(x&7)))
+
+#ifdef WIN32
+static void  xset_bits(u_char *bp,int x,int to,int n) {
+#else
 static inline void xset_bits(u_char *bp,int x,int to,int n) {
+#endif
 	int	i;
 
 	for (i=n;i--;) { SBIT((x+i),(to&1)); to>>=1; }
