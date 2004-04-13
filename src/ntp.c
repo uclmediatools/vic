8da0f40ede337b1227e2a67bb805875d560d9b93
ucaccsp <ucaccsp@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2004-04-13 17:30:38 +0000

    Import Luca Abeni's fix to NTP calculation.

--- src/ntp.c
+++ src/ntp.c
@@ -23,10 +23,12 @@ void
 ntp64_time(uint32_t *ntp_sec, uint32_t *ntp_frac)
 {
         struct timeval now;
+	uint32_t tmp;
 
         gettimeofday(&now, NULL);
 
         /* NB ntp_frac is in units of 1 / (2^32 - 1) secs. */
         *ntp_sec  = now.tv_sec + SECS_BETWEEN_1900_1970;
-        *ntp_frac = (now.tv_usec << 12) + (now.tv_usec << 8) - ((now.tv_usec * 3650) >> 6);
+	tmp = now.tv_usec;
+        *ntp_frac = (tmp << 12) + (tmp << 8) - ((tmp * 3650) >> 6);
 }
