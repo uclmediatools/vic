3f40a69dc096701cf750b1f41d12a7c688c54bee
piers <piers@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2008-10-10 11:55:50 +0000

    Fixed exit() when changing from one codec to another with large size set - it wasn't checking if the deimation had changed if the capture format was the same - so it crashed/exited if codec unable to cope (e.g. large H264 to H261). Reported by Tobias at AGSC.

--- tcl/ui-ctrlmenu.tcl
+++ tcl/ui-ctrlmenu.tcl
@@ -1489,7 +1489,7 @@ set qscale_val(raw) 1
 set lastFmt ""
 
 proc select_format fmt {
-	global qscale qlabel videoDevice videoFormat qscale_val lastFmt
+	global qscale qlabel videoDevice videoFormat qscale_val lastFmt inputSize
 
 	if { $fmt == "h261" || $fmt == "pvh"} {
 		# H.261 supports only QCIF/CIF
@@ -1550,6 +1550,7 @@ proc select_format fmt {
 			# XXX doens't work if title-maker is installed
 			# SV, title-maker fix: see marked code below
 			delete $V(encoder)
+			$V(grabber) decimate $inputSize
 			set V(encoder) $encoder
 
 			update_encoder_param
