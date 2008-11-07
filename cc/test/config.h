94db3bcf276daeecbe9c71c6612941b919941e0a
soohyunc <soohyunc@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2008-11-07 16:06:49 +0000

    setting up unit test codes used for CC mechanisms (curretnly TFWC only)

---
+++ cc/test/config.h
@@ -0,0 +1,13 @@
+/* $Id$ */
+#ifdef vic_cc_test_config_h
+#define vic_cc_test_config_h
+
+#include <stdio.h>      // standard input/output
+#include <stdlib.h>     // general utilities
+#include <string.h>     // string related
+#include <errno.h>      // errors
+#include <stdbool.h>    // support boolean data type
+#include <time.h>       // support sleep function
+#include <sys/types.h>
+
+#endif /* vic_cc_test_config_h */
