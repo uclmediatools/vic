73c65bb5c9b28fa677cf558575aba5d274a98901
piers <piers@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2009-04-09 09:55:44 +0000

    Converted VIC files to 3-clause BSD license - thus now GPL compatible. Since most of the code came from Berkeley most of the changes are covered by the announcement of the advertising clause removal from the official BSD license text on July 22, 1999 by William Hoskins, the director of the office of technology licensing for Berkeley - see:  ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change This conversion work is largely down to Doug Kossivic - thanks!
    For BSD/LBL 4 clause BSD licensed files:
     - Use the same OSI approved 3 clause BSD license as Open Mash which has
    a new disclaimer
     - Add year to the files missing the copyright year
     - For BSD/LBL files (e.g. net/inet.c) that have an earlier license that
    predates the 4 clause BSD license, update to the 3 clause BSD/LBL
    license.
     - Remove BSD/LBL license from files covered by Xerox license, like Open
    Mash did
     - Remove BSD/LBL license from files covered by Stanford University
    Portable Video Research Group public domain license, like Open Mash did
     - Remove BSD/LBL license from Sun CellB encoder/decoder files, like
    Open Mash did
    
    For UCL 4 clause BSD-like licensed files:
     - Removed preamble about permitted for non-commercial use only.
     - Now uses standard OSI approved 3 clause BSD license
     - Obtained permission of UCL copyright holders
    
    Jim Lowe 4 clause BSD licensed files (grabber-meteor.cpp &
    grabber-spigot.cpp)
     - Obtained permission to change to 3 clause
    
    Luigi Rizzo 4 clause BSD licensed grabber-x11.cpp:
     - Obtained permission to change to 3 clause
    
    Marcus Meissner and The Regents of the University of Erlangen 4 clause
    BSD licensed files:
     - Advertising clause was for UCB and Network Research Group at LBL
     - Should be safe to remove as it was removed elsewhere.

--- codec/h263/h263rtp.c
+++ codec/h263/h263rtp.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1998-1999 Marcus Meissner and The Regents of the University of
- * Erlangen.
+ * Copyright (c) 1998-1999 Marcus Meissner and
+ *     The Regents of the University of Erlangen.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,26 +11,21 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and the Network Research Group at
- *      Lawrence Berkeley Laboratory.
- * 4. Neither the name of the University nor of the Laboratory may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
+ * 3. Neither the names of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 /* History:
@@ -39,7 +34,7 @@
  * University of Erlangen-Nuremberg, Lehrstuhl fuer Nachrichtentechnik by
  * Marcus Meissner in 1998.
  *
- * Merged into UCL vic development tree in 1998 by ?
+ * Merged into UCL vic development tree in 1998
  *
  * Cleanups, endianess, translation of comments into english, bugfixes,
  * done by Marcus Meissner in June 1999.
