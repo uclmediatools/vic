77a9dc3b63a09ecb2248187b1b9bbce08cd2929d
soohyunc <soohyunc@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2010-02-08 12:31:29 +0000

    processing trace

--- cc/o.sh
+++ cc/o.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # packet seqno trace in time
-grep seqno trace.s | grep -v pid | awk '{print $2" "$4}' > seqno.tr
+grep seqno trace.s | grep -v pid | awk '{if($1=="now:" && $3=="seqno:") print}' | awk '{print $2" "$4}' > seqno.tr
 cc/tools/asv seqno.tr
 rm seqno.tr
 
@@ -12,4 +12,4 @@ grep h261_encode_start trace.s | grep -v pid | awk '{print $3" "100}' > encs.xg
 grep h261_encode_end trace.s | grep -v pid | awk '{print $3" "100}' > ence.xg
 
 # encoding time difference
-grep enc_time trace.s | grep -v pid | awk '{print $2"\t"$4}' > enct.xg
+grep enc_time trace.s | awk '{if($1=="num:" && $3=="enc_time:") print}' | awk '{print $2"\t"$4}' > enct.xg
