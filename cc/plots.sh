78583d0aa17f2415acba77c1788ef6234d83c349
soohyunc <soohyunc@bc8abe95-590b-0410-a0f3-dc9ed9a1a8fa>
2010-05-13 23:27:08 +0000

    changes for gnuplot script   (when xr_send.xg exists, then plot it)

--- cc/plots.sh
+++ cc/plots.sh
@@ -1,103 +1,169 @@
 #!/bin/sh
 
+#---------------------------------------------------------------------#
+# inter-active plots
+#---------------------------------------------------------------------#
+if [ -e xr_send.xg ]
+then
 gnuplot -persist << EOF
+  set style line 1 lt 1 pt 4 lw 1.5
+  set style line 2 lt 2 pt 4 lw 1.5
+  set style line 3 lt 3 pt 4 lw 1.5
+  set style line 4 lt 4 pt 4 lw 1.5
+  set style line 5 lt 5 pt 4 lw 1.5
 
-set style line 1 lt 1 pt 4 lw 1.5
-set style line 2 lt 2 pt 4 lw 1.5
-set style line 3 lt 3 pt 4 lw 1.5
-set style line 4 lt 4 pt 4 lw 1.5
-set style line 5 lt 5 pt 4 lw 1.5
+  set mxtics 2
+  set mytics 4
+  set grid
 
-set mxtics 2
-set mytics 4
-set grid
+  set xrange [$1:$2]
+  set yrange [$3:$4]
 
-set xrange [$1:$2]
-set yrange [$3:$4]
+  plot \
+	"encs.xg" with impulses title "start encoding", \
+	"ence.xg" with impulses title "end encoding", \
+	"inXR.xg" with impulses title "ackvec arrival", \
+	"txq.xg"  with linespoints lw 2.5 pt 5 title "tx queue", \
+	"cwnd.xg" with linespoints lw 2.5 lc 9 pt 6 title "cwnd", \
+	"seqno.xg" ls 3 title "packet", \
+	"xr_send.xg" using 1:4 with impulses lt 1 lc 0 title "XR sent"
+EOF
+else
+gnuplot -persist << EOF
+  set style line 1 lt 1 pt 4 lw 1.5
+  set style line 2 lt 2 pt 4 lw 1.5
+  set style line 3 lt 3 pt 4 lw 1.5
+  set style line 4 lt 4 pt 4 lw 1.5
+  set style line 5 lt 5 pt 4 lw 1.5
 
-plot \
+  set mxtics 2
+  set mytics 4
+  set grid
+
+  set xrange [$1:$2]
+  set yrange [$3:$4]
+
+  plot \
 	"encs.xg" with impulses title "start encoding", \
 	"ence.xg" with impulses title "end encoding", \
 	"inXR.xg" with impulses title "ackvec arrival", \
 	"txq.xg"  with linespoints lw 2.5 pt 5 title "tx queue", \
 	"cwnd.xg" with linespoints lw 2.5 lc 9 pt 6 title "cwnd", \
 	"seqno.xg" ls 3 title "packet"
+EOF
+fi
 
+#---------------------------------------------------------------------#
+# eps plot
+#---------------------------------------------------------------------#
+if [ -e xr_send.xg ]
+then
+gnuplot -persist << EOF
+  set terminal postscript eps enhanced color
+  set output "seqno.eps"
+
+  set title "time-seqno plot"
+  set xlabel "time (sec)"
+  set ylabel "seqno"
+
+  set mxtics 2
+  set mytics 4
+
+  set xrange [$1:$2]
+  set yrange [$3:$4]
+
+  plot \
+	"encs.xg" with impulses title "start encoding", \
+	"ence.xg" with impulses title "end encoding" lc 4 lt 1, \
+	"inXR.xg" with impulses title "ackvec arrival" lt 1 lc 2, \
+	"txq.xg"  with linespoints lw 2.5 lc 0 lt 1 pt 5 title "tx queue", \
+	"cwnd.xg" with linespoints lw 2.5 lc 9 lt 1 pt 6 title "cwnd", \
+	"seqno.xg" pt 4 lc 3 title "packet", \
+	"xr_send.xg" using 1:4 with impulses lt 1 lc 0 title "XR sent"
+EOF
+else
+gnuplot -persist << EOF
+  set terminal postscript eps enhanced color
+  set output "seqno.eps"
+
+  set title "time-seqno plot"
+  set xlabel "time (sec)"
+  set ylabel "seqno"
+
+  set mxtics 2
+  set mytics 4
+
+  set xrange [$1:$2]
+  set yrange [$3:$4]
+
+  plot \
+	"encs.xg" with impulses title "start encoding", \
+	"ence.xg" with impulses title "end encoding" lc 4 lt 1, \
+	"inXR.xg" with impulses title "ackvec arrival" lt 1 lc 2, \
+	"txq.xg"  with linespoints lw 2.5 lc 0 lt 1 pt 5 title "tx queue", \
+	"cwnd.xg" with linespoints lw 2.5 lc 9 lt 1 pt 6 title "cwnd", \
+	"seqno.xg" pt 4 lc 3 title "packet"
+EOF
+fi
+
+
+#---------------------------------------------------------------------#
+# encoding time related
+#---------------------------------------------------------------------#
+gnuplot -persist << EOF
+  set terminal postscript eps enhanced color
+  set output "enct.eps"
+
+  set title "encoding time"
+  set xlabel "num encoding"
+  set ylabel "time (sec)"
+
+  set mxtics 2
+  set mytics 2
+  set xrange [$1:$2]
+  set yrange [0:]
+
+  plot \
+	"enct.xg" with lp title "encoding time", \
+	"grt.xg" with lp lt 1 lc 3 title "grabbing time"
 EOF
 
+#---------------------------------------------------------------------#
+# cwnd
+#---------------------------------------------------------------------#
 gnuplot -persist << EOF
-set terminal postscript eps enhanced color
-set output "seqno.eps"
+  set terminal postscript eps enhanced color
+  set output "cwnd.eps"
 
-set title "time-seqno plot"
-set xlabel "time (sec)"
-set ylabel "seqno"
+  set title "cwnd"
+  set xlabel "vic run time (sec)"
+  set ylabel "cwnd"
 
-set mxtics 2
-set mytics 4
+  set mxtics 5
+  set mytics 2
+  set xrange [$1:$2]
+  set yrange [0:]
+  set grid xtics ytics mytics
 
-set xrange [$1:$2]
-set yrange [$3:$4]
-
-plot \
-"encs.xg" with impulses title "start encoding", \
-"ence.xg" with impulses title "end encoding" lc 4 lt 1, \
-"inXR.xg" with impulses title "ackvec arrival" lt 1 lc 2, \
-"txq.xg"  with linespoints lw 2.5 lc 0 lt 1 pt 5 title "tx queue", \
-"cwnd.xg" with linespoints lw 2.5 lc 9 lt 1 pt 6 title "cwnd", \
-"seqno.xg" pt 4 lc 3 title "packet"
-
+  plot "cwnd.xg" w lp lc 3 pt 4 ps .4 title "cwnd"
 EOF
 
+#---------------------------------------------------------------------#
+# ALI
+#---------------------------------------------------------------------#
 gnuplot -persist << EOF
-set terminal postscript eps enhanced color
-set output "enct.eps"
+  set terminal postscript eps enhanced color
+  set output "ALI.eps"
 
-set title "encoding time"
-set xlabel "num encoding"
-set ylabel "time (sec)"
+  set title "average loss interval"
+  set xlabel "vic run time (sec)"
+  set ylabel "ALI"
 
-set mxtics 2
-set mytics 2
-set xrange [$1:$2]
-set yrange [0:]
+  set mxtics 5
+  set mytics 2
+  set xrange [0:]
+  set yrange [0:]
+  set grid xtics ytics mytics
 
-plot \
-"enct.xg" with lp title "encoding time", \
-"grt.xg" with lp lt 1 lc 3 title "grabbing time"
-
-EOF
-
-gnuplot -persist << EOF
-set terminal postscript eps enhanced color
-set output "cwnd.eps"
-
-set title "cwnd"
-set xlabel "vic run time (sec)"
-set ylabel "cwnd"
-
-set mxtics 5
-set mytics 2
-set xrange [$1:$2]
-set yrange [0:]
-set grid xtics ytics mytics
-
-plot "cwnd.xg" w lp lc 3 pt 4 ps .4 title "cwnd"
-EOF
-
-gnuplot -persist << EOF
-set terminal postscript eps enhanced color
-set output "ALI.eps"
-
-set title "average loss interval"
-set xlabel "vic run time (sec)"
-set ylabel "ALI"
-
-set mxtics 5
-set mytics 2
-set xrange [0:]
-set yrange [0:]
-set grid xtics ytics mytics
-
-plot "ALI.xg" w lp lc 1 pt 5 ps .4 title "ALI"
+  plot "ALI.xg" w lp lc 1 pt 5 ps .4 title "ALI"
 EOF
